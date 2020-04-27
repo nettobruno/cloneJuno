b088508e712d69f2e4293c4b53d1a301e160aa28
Bruno Netto <brunonettomac@outlook.com>
2020-04-26 22:11:39 -0300

    Layout improvements for smaller devices

--- js/main.js
+++ js/main.js
@@ -1,9 +1,7 @@
 // Menu altera de cor conforme o scroll é ativado
-
 var windowWidth = window.innerWidth;
 
 if (windowWidth > 10) {
-  console.log('Chegoooou');
 	$(window).on('scroll', _.debounce(function () {
 		var $nav = $('nav'),
 			navHeight = $nav.outerHeight(),
@@ -17,8 +15,8 @@ if (windowWidth > 10) {
 	}, 10));
 };
 
-// Configurações para o slide de blocos
 
+// Configurações para o slide de blocos
 $('.slide').slick({
   dots: true,
   infinite: false,
@@ -27,30 +25,11 @@ $('.slide').slick({
   slidesToScroll: 2,
   responsive: [
     {
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 3,
-        slidesToScroll: 3,
-        infinite: true,
-        dots: true
-      }
-    },
-    {
-      breakpoint: 600,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 2
-      }
-    },
-    {
-      breakpoint: 480,
+      breakpoint: 768,
       settings: {
         slidesToShow: 1,
-        slidesToScroll: 1
+        slidesToScroll: 1,
       }
     }
-    // You can unslick at a given breakpoint now by adding:
-    // settings: "unslick"
-    // instead of a settings object
   ]
 });
